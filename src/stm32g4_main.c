ef513e6fb2ba7da1cb4128a170d67fb3e6167b94
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-03-14 20:35:50 +0000

    Add all FreeRTOS files for this branch

--- src/stm32g4_main.c
+++ src/stm32g4_main.c
@@ -1,23 +1,42 @@
 #include "stm32g4_main.h"
 #include "gpio_g431.h"
 
+#include "FreeRTOS.h"
+#include "task.h"
 #include "libopencm3/stm32/rcc.h"
 #include "libopencm3/stm32/gpio.h"
 
+// Must define when using a non-zero config for stack overflow
+void vApplicationStackOverflowHook(
+         xTaskHandle pxTask __attribute((unused))
+         , char *pcTaskName __attribute((unused)))
+{
+	for (;;) {
+		// do nothing
+	}
+}
+
+
+static void task1(void *args __attribute((unused)))
+{
+	for (;;) {
+		gpio_toggle(LED_PORT, LED_PIN);
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
+}
+
+
 int main (void)
 {
+	rcc_set_hpre(RCC_CFGR_HPRE_DIV2); // HCLK = SYSCLK / 2 (16 MHz / 2)
+
 	gpio_setup();
 
-	int i;
+	xTaskCreate(task1, "Flash_LED", 100, NULL, configMAX_PRIORITIES-1, NULL);
+	vTaskStartScheduler();
+
 	for (;;) {
-		gpio_clear(LED_PORT, LED_PIN);
-		for (i = 0; i < 1500000; i++) {
-			__asm__("nop");
-		}
-		gpio_set(LED_PORT, LED_PIN);
-		for (i = 0; i < 500000; i++) {
-			__asm__("nop");
-		}
+		// FreeRTOS should never let us execute or return from main
 	}
 
 	return 0;
