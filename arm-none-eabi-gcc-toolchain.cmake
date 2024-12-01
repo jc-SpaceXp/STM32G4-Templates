7e0b244f4fe583c3b54febba61e59fdc4b13862a
Jamal Clarke <jamal_clarke@hotmail.co.uk>
2024-12-01 21:45:22 +0000

    Squashed commit of the following:
    commit bf0057a3b97ebfffcd6d89b72436a9327dd7f5c6
    Author: Jamal Clarke <jamal_clarke@hotmail.co.uk>
    Date:   Sun Dec 1 21:44:10 2024 +0000
    
        Update readme from latest cmake branch merge
    
    commit 1dc8465e685ec4e54a234ef7bed933da31ebaff7
    Author: Jamal Clarke <jamal_clarke@hotmail.co.uk>
    Date:   Sun Dec 1 21:42:29 2024 +0000
    
        Fix broken cmake file
    
        - Flash commands must belong to the stm32 preset section otherwise unit
          tests will not build
    
    commit ea2b6254d9e4abe93d0ceb4abc1f00b95147522e
    Author: Jamal Clarke <jamal_clarke@hotmail.co.uk>
    Date:   Sun Dec 1 21:38:40 2024 +0000
    
        Add support for user configurable rtos heap
    
        - Pass -DRTOSHEAPCONFIG=x to pass a value other than 4 during generation
    
    commit 46454cfefb2cc95f04922b1592403e2d494de1c2
    Author: Jamal Clarke <jamal_clarke@hotmail.co.uk>
    Date:   Sun Dec 1 21:36:43 2024 +0000
    
        Support freertos builds with cmake
    
        - Tested, works
    
    commit 3ef4266bf0cfcab8f143bd6b88de873fdefe66df
    Author: Jamal Clarke <jamal_clarke@hotmail.co.uk>
    Date:   Sun Dec 1 21:19:16 2024 +0000
    
        Pull in cmake changes from other cmsis branch

---
+++ arm-none-eabi-gcc-toolchain.cmake
@@ -0,0 +1,18 @@
+set(CROSS_TOOLCHAIN arm-none-eabi)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_C_COMPILER ${CROSS_TOOLCHAIN}-gcc CACHE INTERNAL "C compiler")
+set(CMAKE_ASM_COMPILER ${CROSS_TOOLCHAIN}-as CACHE INTERNAL "ASM compiler")
+set(CMAKE_OBJCOPY ${CROSS_TOOLCHAIN}-objcopy CACHE INTERNAL "Objcopy tool")
+set(CMAKE_SIZE_UTIL ${CROSS_TOOLCHAIN}-size CACHE INTERNAL "Size tool")
+
+set(CPUFLAGS "-mcpu=cortex-m4;-mthumb")
+set(FPUFLAGS "-mfloat-abi=hard;-mfpu=fpv4-sp-d16")
+set(SIZEFLAGS "-ffunction-sections;-fdata-sections")
+
+set(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/STM32G431KBTX_FLASH.ld)
+set(LINKER_NOSYS -Wl,--start-group -lc -lgcc -lnosys -Wl,--end-group)
+set(LINKER_MAPFILE -Wl,-Map=main.map,--cref)
